ef55f6c8ebdbe0d0cbe3f50bfa48e0b4fc7f0682
Leysen95 <mullayanova95@bk.ru>
2024-10-24 18:21:31 +0500

    initial commit

---
+++ src/test/java/me/skypro/Calculator/Homework_2/_2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.skypro.Calculator.Homework_2._2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
